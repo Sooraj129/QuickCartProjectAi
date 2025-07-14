3f4484a07216540f72b2064861f38685f7c4305c
Soorajss1 <soorajcrazy2001@gmail.com>
2025-07-14 23:22:41 +0530

    Changes in AiTools component

---
+++ Client/src/components/AiTools/AiTools.tsx
@@ -0,0 +1,15 @@
+
+
+const AiTools:React.FC = () => {
+  return (
+    <div className="px-4 sm:px-20 xl:px-32 my-24">
+        <div className="text-center">
+            <h2>PowerFul Ai tools</h2>
+            <p>Everything you need to create,enhance and optimize your content</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default AiTools
--- Client/src/pages/Home/Home.tsx
+++ Client/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
+import AiTools from "@/components/AiTools/AiTools";
 
 const Home: React.FC = () => {
   return (
     <div>
       <Navbar />
        <Hero/>
+       <AiTools/>
     </div>
   );
 };
